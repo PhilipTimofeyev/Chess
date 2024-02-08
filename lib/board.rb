003f264c403d56d6d633bfdc015ac1eb30a2df30
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-02-08 10:21:16 -0800

    Removed 'Square' class and slightly refactored 'Chessboard' class

--- lib/board.rb
+++ lib/board.rb
@@ -1,31 +1,20 @@
 
-
-class Square
-  EMPTY_SQUARE = '_'
-
-  attr_accessor :chess_piece
-  attr_reader :coordinate
-
-  def initialize
-    @chess_piece = EMPTY_SQUARE
-  end
-end
-
 class Chessboard
+  EMPTY_SQUARE = '_'
   LOWER_BOUND = 0
   UPPER_BOUND = 7
   LETTERS = %w(A B C D E F G H)
 
-  attr_accessor :board
+  attr_accessor :squares
 
   def initialize
-    @board = {}
+    @squares = {}
   end
 
   def build_board
     (LOWER_BOUND + 1..UPPER_BOUND + 1).each do |row|
       LETTERS.each do |column|
-        board[(column.to_s + row.to_s).to_sym] = Square.new
+        squares[(column.to_s + row.to_s).to_sym] = EMPTY_SQUARE
       end
     end
   end
@@ -33,8 +22,8 @@ class Chessboard
   def display_board
     row_count = UPPER_BOUND
 
-    board.values.reverse.each_slice(UPPER_BOUND + 1) do |row| 
-      puts "#{row_count + 1}|#{row.reverse.map{|square| square.chess_piece}.join("|")}|" 
+    squares.values.reverse.each_slice(UPPER_BOUND + 1) do |row| 
+      puts "#{row_count + 1}|#{row.reverse.map{|square| square}.join("|")}|" 
       row_count -= 1
     end
 
@@ -45,4 +34,3 @@ class Chessboard
     puts "  " + LETTERS.map{|n| n.to_s}.join(" ")
   end
 end
-
