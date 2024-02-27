b302c949832789289e5163220f7b736b9190f1fe
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-02-27 14:00:56 -0800

    Corrected stalemate? method use

--- main.rb
+++ main.rb
@@ -6,18 +6,18 @@ require_relative './lib/player'
 board = Chessboard.new
 board.build_empty_board
 board.set_board_pieces
+board.to_s
 player_one = Player.new
 player_one.select_color
 player_two = Player.new
 player_two.color = player_one.color == :white ? :black : :white
 
 players = [player_one, player_two]
-board.to_s
 
 loop do
+  break if board.checkmate? || board.stalemate?(players.first.color)
   players.first.turn(board)
   board.to_s
-  break if board.checkmate? || board.stalemate?
   players.reverse!
 end
 
